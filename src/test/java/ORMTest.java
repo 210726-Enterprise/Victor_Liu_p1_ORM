d32fc8e58b6f678252087cd849e070dda77f4e84
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-16 20:03:07 -0400

    initial test cases

---
+++ src/test/java/ORMTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+public class ORMTest
+{
+    @Test
+    public void getConnectionTest()
+    {
+
+    }
+
+    @Test
+    public void retrieveORMTest()
+    {
+
+    }
+
+    @Test
+    public void createORMTest()
+    {
+
+    }
+
+    @Test
+    public void deleteORMTest()
+    {
+
+    }
+
+    @Test
+    public void updateORMTest()
+    {
+
+    }
+
+}
