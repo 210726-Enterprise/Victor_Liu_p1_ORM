ef5f4f0b114a064426a88f13a2187075fb09f966
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-20 15:12:34 -0400

    groundwork for servlet and service

---
+++ Project 1 - Servlet/src/main/java/com/revature/service/VideoGameService.java
@@ -0,0 +1,18 @@
+package com.revature.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VideoGameService {
+    public void getVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    public void insertVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    public void updateVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    public void deleteVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    }
+}
