e5afc04e7268f11e2f412b3c18fca54ce4cf0a67
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-22 02:14:23 -0400

    implemented logging in service

--- Project 1 - Servlet/src/main/java/com/revature/service/VideoGameService.java
+++ Project 1 - Servlet/src/main/java/com/revature/service/VideoGameService.java
@@ -1,18 +1,41 @@
 package com.revature.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.orm.util.ConfigBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class VideoGameService {
-    public void getVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+public class VideoGameService
+{
+    private static final Logger logger = LoggerFactory.getLogger(VideoGameService.class);
+
+    private ObjectMapper objectMapper;
+
+    public VideoGameService()
+    {
+        objectMapper = new ObjectMapper();
     }
 
-    public void insertVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    public void getVideoGame(HttpServletRequest req, HttpServletResponse resp)
+    {
+
     }
 
-    public void updateVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    public void insertVideoGame(HttpServletRequest req, HttpServletResponse resp)
+    {
+
     }
 
-    public void deleteVideoGame(HttpServletRequest req, HttpServletResponse resp) {
+    public void updateVideoGame(HttpServletRequest req, HttpServletResponse resp)
+    {
+
+    }
+
+    public void deleteVideoGame(HttpServletRequest req, HttpServletResponse resp)
+    {
+
     }
 }
