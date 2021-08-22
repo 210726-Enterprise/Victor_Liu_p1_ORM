7180af9f22a153ce9edfb8acfeb1926a66f3ee45
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-22 02:21:50 -0400

    Created video game model

---
+++ Project 1 - Servlet/src/main/java/com/revature/model/VideoGame.java
@@ -0,0 +1,86 @@
+package com.revature.model;
+
+import com.revature.orm.annotations.Column;
+import com.revature.orm.annotations.MetamodelConstructor;
+import com.revature.orm.annotations.PrimaryKey;
+import com.revature.orm.annotations.Table;
+
+@Table(tableName = "VideoGames")
+public class VideoGame
+{
+    @Column(columnName = "id")
+    @PrimaryKey(primaryKeyName = "id")
+    private int id;
+
+    @Column(columnName = "name")
+    private String name;
+
+    @Column(columnName = "developer")
+    private String developer;
+
+    @Column(columnName = "publisher")
+    private String publisher;
+
+    @Column(columnName = "genre")
+    private String genre;
+
+    @MetamodelConstructor
+    public VideoGame(int id, String name, String developer, String publisher, String genre)
+    {
+        this.id = id;
+        this.name = name;
+        this.developer = developer;
+        this.publisher = publisher;
+        this.genre = genre;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDeveloper()
+    {
+        return developer;
+    }
+
+    public void setDeveloper(String developer)
+    {
+        this.developer = developer;
+    }
+
+    public String getPublisher()
+    {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher)
+    {
+        this.publisher = publisher;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+}
