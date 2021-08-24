3c6c044cd2af0a52f430b9462df2aaa6c8d96d84
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-23 21:58:07 -0400

    removed unnecessary import

--- Project 1 - Servlet/src/test/java/com/revature/servlet/VideoGameServletTest.java
+++ Project 1 - Servlet/src/test/java/com/revature/servlet/VideoGameServletTest.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class VideoGameServletTest
